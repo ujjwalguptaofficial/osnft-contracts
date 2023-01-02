0177b3f0340575d15c6d0bcce3f886813b0407fe
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-01-02 11:25:00 +0530

    add optimizer in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
-
 import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: process.env.NODE_ENV === 'production' ? {
+        enabled: true,
+        runs: 1000,
+      } : {},
+    }
+  },
   gasReporter: {
     currency: 'MATIC',
     gasPrice: 41,
-  }
+  },
+
 };
 
 export default config;
