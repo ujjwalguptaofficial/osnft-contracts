73ba3eb62d65d1484e206ce1420413b67fe3513c
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-01-22 21:01:12 +0530

    deploy approver

---
+++ scripts/deploy_approver.ts
@@ -0,0 +1,32 @@
+import { ethers, upgrades } from "hardhat";
+const { LedgerSigner } = require("@anders-t/ethers-ledger");
+
+// import { LedgerSigner } from "@ethersproject/hardware-wallets";
+
+async function main() {
+    const ledger = new LedgerSigner(ethers.provider);
+    const contract = await ethers.getContractFactory('OSNFTApprover');
+
+    const contractFactory = contract.connect(ledger);
+
+    const deployedContract = await upgrades.deployProxy(
+        contractFactory,
+        [
+
+        ],
+        {
+            initializer: 'initialize'
+        }
+    );
+
+    await deployedContract.deployed();
+
+    console.log("Contract deployed to:", deployedContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
