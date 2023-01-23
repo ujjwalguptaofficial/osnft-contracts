ddd8666f88f98492fc66b39e5d4692e5cede8315
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-01-23 20:29:48 +0530

    add marketplace relayer, nft relayer

---
+++ scripts/add_marketplace_relayer.ts
@@ -0,0 +1,35 @@
+import { expect } from "chai";
+import { ethers, upgrades } from "hardhat";
+const { LedgerSigner } = require("@anders-t/ethers-ledger");
+
+// import { LedgerSigner } from "@ethersproject/hardware-wallets";
+
+async function main() {
+    const ledger = new LedgerSigner(ethers.provider);
+
+    const marketplaceAddress = process.env.MARKETPLACE_ADDRESS;
+
+    const contract = await ethers.getContractFactory('OSNFTMarketPlace');
+
+    const contractFactory = contract.connect(ledger);
+
+    const contractInstance = await contractFactory.attach(marketplaceAddress as string);
+
+    const relayerAddress = process.env.RELAYER_ADDRESS as string;
+
+    let relayer = await contractInstance["relayer()"]();
+    expect(relayer).equal(ethers.constants.AddressZero);
+
+    const tx = await contractInstance["relayer(address)"](relayerAddress);
+    await tx.wait();
+
+    relayer = await contractInstance["relayer()"]();
+    expect(relayer).equal(relayerAddress);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
